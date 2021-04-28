a7f5891eac911394ae8c83dd88ec510fd7c88782
dke <artenisfaul@mail.ru>
2021-04-28 08:29:23 +0300

    auto deploy

---
+++ undertool/precache-manifest.b7ab95d5930f2a0a660572e0ffbe11e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a32aca0e6280ddfd4132a51287ae352",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a4aa34094c1668fb9266",
+    "url": "./static/css/main.d9bbe5ed.chunk.css"
+  },
+  {
+    "revision": "8c0e825c16ad5f345313",
+    "url": "./static/js/2.d845f563.chunk.js"
+  },
+  {
+    "revision": "05ea761b7a5361472988577479d7ab28",
+    "url": "./static/js/2.d845f563.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4aa34094c1668fb9266",
+    "url": "./static/js/main.5a06ee0b.chunk.js"
+  },
+  {
+    "revision": "c9f675e552787b84f9eb",
+    "url": "./static/js/runtime-main.f0354cb7.js"
+  }
+]);
